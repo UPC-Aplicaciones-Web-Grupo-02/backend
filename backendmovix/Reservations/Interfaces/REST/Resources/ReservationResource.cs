4f73bda39d982fbebcd878370ced9f0d2231a10a
RafaelCuya <u201913495@upc.edu.pe>
2025-06-16 23:36:45 -0500

    feat: Added ReservationResource.cs

--- backendmovix/Reservations/Interfaces/REST/Resources/ReservationResource.cs
+++ backendmovix/Reservations/Interfaces/REST/Resources/ReservationResource.cs
@@ -2,5 +2,9 @@
 
 public class ReservationResource
 {
-    
+    public int Id { get; set; }
+    public string CantDate { get; set; }
+    public int ScooterId { get; set; }
+    public int UserId { get; set; }
+    public int SuscriptionId { get; set; }
 }
