494e970676c27caae1ce65a730ffb61997430291
Alonso Muñiz <alonoso.percy@gmail.com>
2025-06-17 00:07:21 -0500

    feat: I created the table TypeSubscriptions

--- backendmovix/Suscriptions/Domain/Model/Aggregate/TypeSuscription.cs
+++ backendmovix/Suscriptions/Domain/Model/Aggregate/TypeSuscription.cs
@@ -2,5 +2,13 @@
 
 public class TypeSuscription
 {
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int Id { get; set; }
+    [Required]
+    public string Name { get; set; }
+    [Required]
+    public string Costo { get; set; }
+    public ICollection<Suscription> Suscriptions { get; set; }
     
 }
