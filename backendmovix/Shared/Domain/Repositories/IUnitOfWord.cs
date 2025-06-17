1b9eb864912e3512576c8366f2d868b903238b86
Nestor Velarde <u20211c221@upc.edu.pe>
2025-06-17 02:16:09 -0500

    feat: Agragte Interface IUnitOfWork

---
+++ backendmovix/Shared/Domain/Repositories/IUnitOfWord.cs
@@ -0,0 +1,12 @@
+﻿namespace backendmovix.Shared.Domain.Repositories;
+/// <summary>
+///     Unit of work interface for all repositories
+/// </summary>
+public interface IUnitOfWord
+{
+    /// <summary>
+    ///     Save changes to the repository
+    /// </summary>
+    /// <returns></returns>
+    Task CompleteAsync();
+}
