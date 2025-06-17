716edb41be218780ae60a5fc0c436b945ac0ccbb
Arnold24x24 <argamoso24@hotmail.com>
2025-06-16 23:57:41 -0500

    feat: commit git aggregate suscription.cs

--- backendmovix/Suscriptions/Domain/Model/Aggregate/Suscription.cs
+++ backendmovix/Suscriptions/Domain/Model/Aggregate/Suscription.cs
@@ -2,5 +2,16 @@
 
 public class Suscription
 {
-    
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int Id { get; set; }
+    [Required]
+    public string Number { get; set; }
+    [Required]
+    public string Date { get; set; }
+    [Required]
+    public string Cvv { get; set; }
+    [Required]
+    public int TypeId { get; set; }
+    public TypeSuscription Type { get; set; }
 }
