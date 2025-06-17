8c9b7f439dff150832998d19b0209f9e56863f5d
Nestor Velarde <u20211c221@upc.edu.pe>
2025-06-17 03:15:51 -0500

    Configurations Programs.cs

--- backendmovix/Program.cs
+++ backendmovix/Program.cs
@@ -1,23 +1,54 @@
+using backendmovix.Reservations.Applications.Internal.Service;
+using backendmovix.Scooter.Application.Internal.Service;
+using backendmovix.Shared.Infrastructure.Persistence.EFC.Configuration;
+using backendmovix.Suscriptions.Application.Internal.Service;
+using backendmovix.Suscriptions.Domain.Model.Aggregate;
+using backendmovix.Users.Application.Internal.Service;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();    
+builder.Services.AddSwaggerGen();              
+builder.Services.AddScoped<IScooterService, ScooterService>();
+builder.Services.AddScoped<IUserService, UserService>();
+builder.Services.AddScoped<ISuscriptionService, SuscriptionService>();
+builder.Services.AddScoped<IReservationService, ReservationService>();
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseMySQL(connectionString);
+});
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+using (var scope = app.Services.CreateScope())  
 {
-    app.MapOpenApi();
+    var services = scope.ServiceProvider;
+    var context = services.GetRequiredService<AppDbContext>();
+    var userService = services.GetRequiredService<IUserService>();
+
+    context.Database.EnsureCreated();
+    if (!context.TypeSuscriptions.Any())
+    {
+        context.TypeSuscriptions.AddRange(
+            new TypeSuscription { Name = "Plan Semanal", Costo = "29.90" },
+            new TypeSuscription { Name = "Plan Mensual", Costo = "79.90" },
+            new TypeSuscription { Name = "Plan Trimestral", Costo = "109.90" }
+        );
+        context.SaveChanges();
+    }
 }
-
+app.UseSwagger(); 
+app.UseSwaggerUI(); 
 app.UseHttpsRedirection();
-
+app.UseRouting();
+app.UseCors(builder =>
+    builder.WithOrigins("http://localhost:5184")
+        .AllowAnyHeader()
+        .AllowAnyMethod());
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
