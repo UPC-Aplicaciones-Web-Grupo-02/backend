fffebc656ac36304241aa2574f3c0eb0f28e93ff
RafaelCuya <u201913495@upc.edu.pe>
2025-06-16 23:21:42 -0500

    feat(backend) Added reservation.cs

--- backendmovix/Reservations/Domain/Model/Aggregate/Reservation.cs
+++ backendmovix/Reservations/Domain/Model/Aggregate/Reservation.cs
@@ -2,5 +2,22 @@
 
 public class Reservation
 {
+  [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int Id { get; set; }
     
+    [Required]
+    public string CantDate { get; set; }
+    
+    [Required]
+    public int ScooterId { get; set; }
+    public Scooter.Domain.Model.Aggregate.Scooter Scooter { get; set; }
+    
+    [Required]
+    public int UserId { get; set; }
+    public User User { get; set; }
+    
+    [Required]
+    public int SuscriptionId { get; set; }
+    public Suscription Suscription { get; set; }  
 }
