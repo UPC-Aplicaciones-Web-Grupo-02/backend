02e4eeae7ff430bf20c19826345858f309dbb025
RafaelCuya <u201913495@upc.edu.pe>
2025-06-16 23:28:27 -0500

    feat: ReservationService.cs

--- backendmovix/Reservations/Applications/Internal/Service/ReservationService.cs
+++ backendmovix/Reservations/Applications/Internal/Service/ReservationService.cs
@@ -1,6 +1,23 @@
 ﻿namespace backendmovix.Reservations.Applications.Internal.Service;
 
-public class ReservationService
+public class ReservationService 
 {
-    
+    private readonly AppDbContext _context;
+
+    public ReservationService(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<IEnumerable<Reservation>> ListAsync()
+    {
+        return await _context.Reservations.ToListAsync();
+    }
+
+    public async Task<Reservation> CreateAsync(Reservation reservation)
+    {
+        _context.Reservations.Add(reservation);
+        await _context.SaveChangesAsync();
+        return reservation;
+    }
 }
